04adfe08a76e3aed2b8ee5e7e15a52dc0b5d88cb
40wa <0000wangalexander@gmail.com>
2020-12-06 13:10:39 +0000

    Completed Day 6.

---
+++ Day6/soln.py
@@ -0,0 +1,14 @@
+from functools import reduce
+
+def main():
+    with open('data') as f: data = f.read()
+    
+    # Part One
+    print("Part One")
+    print(reduce(lambda cnt,ls: cnt + len(set(ls.replace('\n',''))), data.split('\n\n'), 0))
+
+    print("Part Two")
+    print(reduce(lambda cnt,ls: cnt + len(reduce(lambda s,l: set(s).intersection(set(l)), ls.split())), data.split('\n\n'), 0))
+
+if __name__=="__main__":
+    main()

f3dc86c6b371e6024995f8f9bf30a4fd5cf1f414
40wa <0000wangalexander@gmail.com>
2020-12-06 13:24:07 +0000

    Formatting

--- Day6/soln.py
+++ Day6/soln.py
@@ -5,10 +5,17 @@ def main():
     
     # Part One
     print("Part One")
-    print(reduce(lambda cnt,ls: cnt + len(set(ls.replace('\n',''))), data.split('\n\n'), 0))
+    print(reduce(lambda cnt,ls: cnt + len(set(ls.replace('\n',''))),
+                 data.split('\n\n'),
+                 0))
 
     print("Part Two")
-    print(reduce(lambda cnt,ls: cnt + len(reduce(lambda s,l: set(s).intersection(set(l)), ls.split())), data.split('\n\n'), 0))
+    print(reduce(lambda cnt,ls: 
+                     cnt + len(reduce(
+                                      lambda s,l: set(s).intersection(set(l)),
+                                      ls.split())),
+                 data.split('\n\n'),
+                 0))
 
 if __name__=="__main__":
     main()

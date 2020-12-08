8f9020f176da6904a6deabdc6802bcc4500179f3
40wa <0000wangalexander@gmail.com>
2020-12-08 18:46:07 +0000

    Updated bigboy. Bigboy is dead, longlive Bigbo

--- Day8/soln.py
+++ Day8/soln.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 def iter_run(instrs, posacc, lookaheads=None):
     pos,accum = posacc[0], posacc[1]
@@ -28,29 +29,37 @@ def other():
     
     # Part One
     print("Part One")
-    with open('data') as f: data = f.readlines()
+    with open('bigboy') as f: data = f.readlines()
 
+    p1_a = time.perf_counter()
     posacc = [0,0]
     cpu = (0,0,0)
     lookahead = set()
     while cpu[0] != 1:
         cpu = iter_run(data, posacc, lookahead)
-        posacc[0],posacc[1] = cpu[1], cpu[2]
+        posacc[0],posacc[1] = cpu[1],cpu[2]
 
+    p1_b = time.perf_counter()
     print(cpu[2])
+    print('elapsed: ', p1_b - p1_a)
 
     # Part Two
     print("Part Two")
+    p1_a = time.perf_counter()
     for l in lookahead:
         cpu = (0,*l)
         posacc = [l[0], l[1]] 
         while cpu[0] == 0:
             cpu = iter_run(data, posacc)
-            posacc[0],posacc[1] = cpu[1], cpu[2]
+            posacc[0],posacc[1] = cpu[1],cpu[2]
 
         if cpu[0] == 2:
+            p1_b = time.perf_counter()
             print(cpu[2])
             break
+
+    print('elapsed: ', p1_b - p1_a)
+
 
 if __name__=="__main__":
     other()
